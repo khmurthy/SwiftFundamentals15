d6af33854c5e11fa9bd015a48500cfba86d85538
khmurthy <khmurthy6@gmail.com>
2025-01-23 14:23:14 -0500

    Filled out Q&A's for this assignment

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -3,21 +3,21 @@
 
  The code below prints a few short statements about what you have learned in this lesson. Open the console area and view the code's output.
  */
-print("I have learned the following:")
-print("What features make Swift a modern and safe language")
-print("How to use the Swift REPL in Terminal")
-print("How to use playgrounds to make writing Swift fun and simple")
+print("I have learned the following: How to use Xcode and Swift playgrounds")
+print("\nWhat features make Swift a modern and safe language? The syntax is clean, and it has many features that make it easier to work with. For example, optionals are a way to handle missing values, and functions can have multiple return values. Additionally, Swift has a powerful type system and error handling, to prevent crashes or unexpected behaviors.")
+print("\nHow to use the Swift REPL in Terminal: open the terminal, type \"swift repl\", and press enter to enter the Swift REPL. Then you can type code and press enter to run it.")
+print("\nHow to use playgrounds to make writing Swift fun and simple: Playgrounds lets you test code as you write it, offering instant feedback and a clean environment to experiment with new ideas.")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
-
+print("\n Title: Ma Meilleure Ennemie")
+print("Artist: Stromae & Pomme")
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
-
-
+print("\n🎵T'es la meilleure chose qui m'est arrivee")
+print("Mais aussi la pire chose qui m'est arrivee")
+print("Ce jour ou je t'ai rencontree, j'aurais peut-etre prefere...")
 /*:
 
  _Copyright © 2023 Apple Inc._
