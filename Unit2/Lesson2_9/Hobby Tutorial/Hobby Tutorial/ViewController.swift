6c93c7f315ddc3689029711c1aa719c2a2ea5574
khmurthy <khmurthy6@gmail.com>
2025-03-02 07:55:27 -0500

    Created view test
    Created app that uses image views and labels to display basic information about chess, i.e. the names of the pieces

---
+++ Unit2/Lesson2_9/Hobby Tutorial/Hobby Tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hobby Tutorial
+//
+//  Created by Susan Murthy on 3/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
